bb9e432b001caf4b328f1d73f0dea2911b9809d4
lostimagination <diva.tk@ukr.net>
2024-12-29 13:59:10 +0100

    footer added

--- template-parts/builder/company_logos.php
+++ template-parts/builder/company_logos.php
@@ -1,25 +1,38 @@
 <?php
 $logo_company      = get_sub_field( 'logo_company' );
 $sup_title_section = get_sub_field( 'sup_title_section' );
-$background_color = get_sub_field( 'background_color' ) ? 'bg-biege-450' : '';
+$background_color  = get_sub_field( 'background_color' ) ? 'bg-beige-450' : '';
 ?>
 <section class="company-logos <?php echo $background_color ?>">
 	<div class="container">
 		<?php if ( $sup_title_section ) : ?>
 			<span class="h5 sup-title-section"><?php echo $sup_title_section ?></span>
 		<?php endif; ?>
-		<div class="gallery">
-			<?php if ( $logo_company ): ?>
-				<?php foreach ( $logo_company as $image ): ?>
+		<div class="gallery-slider-wrapper">
+			<div class="swiper slide-gallery">
+				<div class="swiper-wrapper">
+					<?php if ( $logo_company ): ?>
+						<?php foreach ( $logo_company as $image ): ?>
 
-					<div class="columns small-12 medium-6 large-4">
-						<img class="img-responsive" src="<?php echo $image['url']; ?>"
-							 alt="<?php echo $image['alt']; ?>"/>
-						<p><?php echo $image['caption']; ?></p>
-					</div>
+							<div class="swiper-slide">
+								<img class="img-responsive" src="<?php echo $image['url']; ?>"
+									 alt="<?php echo $image['alt']; ?>"/>
+							</div>
 
-				<?php endforeach; ?>
-			<?php endif; ?>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</div>
+			</div>
+			<div class="swiper-button-next">
+				<svg>
+					<use xlink:href="#arrow-right-next"></use>
+				</svg>
+			</div>
+			<div class="swiper-button-prev">
+				<svg>
+					<use xlink:href="#arrow-left-prev"></use>
+				</svg>
+			</div>
 		</div>
 	</div>
 </section>
--- template-parts/builder/testimonials.php
+++ template-parts/builder/testimonials.php
@@ -1,21 +1,57 @@
-<?php
-$image_section = get_sub_field( 'image_section' );
-$quote_text = get_sub_field('quote_text');
-?>
 <section class="testimonials">
 	<div class="container">
-		<div class="row">
-			<div class="col-lg-5">
-				<?php if ( $image_section ) : ?>
-					<div class="image-wrapper">
-						<?php echo wp_get_attachment_image( $image_section['id'], 'medium', false, array( 'class' => 'person-image' ) ); ?>
-					</div>
-				<?php endif; ?>
+
+		<div class="testimonials-slider-wrapper">
+			<div class="swiper slide-testimonials">
+				<div class="swiper-wrapper">
+					<?php
+
+					$testimonials = get_sub_field( 'testimonials' );
+
+					if ( $testimonials ): ?>
+
+						<?php foreach ( $testimonials as $post ): ?>
+							<?php setup_postdata( $post ); ?>
+							<?php
+							$bg_colors = [
+								'turquois' => 'bg-turquois-50',
+								'beige'    => 'bg-beige-450',
+								'purple'   => 'bg-purple-50',
+								'primary'  => 'bg-primary-150',
+							];
+
+							$bg_color         = get_sub_field( 'background_color' );
+							$background_color = $bg_colors[ $bg_color ] ?? '';
+							?>
+							<div class="swiper-slide ">
+								<div class="testimonial-wrapper ">
+									<div class="testimonial-wrapper__color <?php echo $background_color ?>"></div>
+									<div class="testimonial-image">
+										<?php the_post_thumbnail( 'medium', array( 'class' => 'flex-image person-image' ) ); ?>
+									</div>
+									<div class="testimonial-content-wrapper">
+										<svg class="quote-svg">
+											<use xlink:href="#quote"></use>
+										</svg>
+										<div class="testimonial-content"><?php the_content(); ?></div>
+									</div>
+								</div>
+							</div>
+						<?php endforeach; ?>
+
+						<?php wp_reset_postdata(); ?>
+					<?php endif; ?>
+				</div>
 			</div>
-			<div class="col-lg-6 offset-lg-1">
-				<?php if($quote_text) : ?>
-				 <div><?php echo wp_kses_post ($quote_text) ?></div>
-				<?php endif; ?>
+			<div class="swiper-button-next">
+				<svg>
+					<use xlink:href="#arrow-right-next"></use>
+				</svg>
+			</div>
+			<div class="swiper-button-prev">
+				<svg>
+					<use xlink:href="#arrow-left-prev"></use>
+				</svg>
 			</div>
 		</div>
 	</div>
